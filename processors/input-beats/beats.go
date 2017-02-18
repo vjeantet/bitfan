f6e3fc96e460738f8ee6141bf83ae561816ab33b
Valere JEANTET <valere.jeantet@gmail.com>
2017-02-19 00:44:46 +0100

    fix #19 - beats input now rely on elastic/go-lumber server implementation

--- processors/input-beats/beats.go
+++ processors/input-beats/beats.go
@@ -1,4 +1,3 @@
-//go:generate bitfanDoc
 package beatsinput
 
 import (
@@ -6,113 +5,168 @@ import (
 	"crypto/x509"
 	"fmt"
 	"io/ioutil"
-	"net"
-	"sync"
+
 	"time"
 
+	"github.com/elastic/go-lumber/log"
+	"github.com/elastic/go-lumber/server/v2"
 	"github.com/vjeantet/bitfan/processors"
 )
 
-func (p *processor) serve() error {
-	ln, err := net.Listen("tcp", fmt.Sprintf("%s:%d", p.opt.Host, p.opt.Port))
-	if err != nil {
-		return fmt.Errorf("Listener failed: %v", err)
-	}
+func New() processors.Processor {
+	return &processor{opt: &options{}}
+}
 
-	clientTerm := make(chan bool)
-	var wg sync.WaitGroup
-	for {
-		select {
-		case <-p.q:
-			ln.Close()
-			close(clientTerm)
-			wg.Wait()
-			close(p.q)
-			return nil
-		default:
-		}
+type processor struct {
+	processors.Base
 
-		if l, ok := ln.(*net.TCPListener); ok {
-			l.SetDeadline(time.Now().Add(1 * time.Second))
-		}
+	server *v2.Server
+	opt    *options
+	q      chan bool
+}
 
-		conn, err := ln.Accept()
+type options struct {
+	Add_field map[string]interface{}
+	Codec     string
+
+	// The number of seconds before we raise a timeout,
+	// this option is useful to control how much time to wait if something is blocking
+	// the pipeline
+	Congestion_threshold int
+
+	// The IP address to listen on
+	Host string
+
+	// The port to listen on (default 5044)
+	Port int
+
+	// Events are by default send in plain text,
+	// you can enable encryption by using ssl to true and
+	// configuring the ssl_certificate and ssl_key options
+	Ssl bool
+
+	// SSL certificate to use (path)
+	Ssl_certificate string
+
+	// Validate client certificates against theses authorities
+	//  You can defined multiples files or path, all the certificates will be read
+	//  and added to the trust store.
+	//  You need to configure the ssl_verify_mode to peer or force_peer to enable
+	//  the verification.
+	// This feature only support certificate directly signed by your root ca.
+	// Intermediate CA are currently not supported.
+	Ssl_certificate_authorities []string
+
+	// SSL key to use (path)
+	Ssl_key string
+
+	// SSL key passphrase to use. (not yet implemented)
+	Ssl_key_passphrase string
+
+	// By default the server dont do any client verification,
+	// peer will make the server ask the client to provide a certificate,
+	//   if the client provide the certificate it will be validated.
+	// force_peer will make the server ask the client for their certificate,
+	//   if the clients doesn’t provide it the connection will be closed.
+	// This option need to be used with ssl_certificate_authorities and a defined list of CA.
+	// Value can be any of: none, peer, force_peer
+	Ssl_verify_mode string
+
+	// Add any number of arbitrary tags to your event
+	Tags []string
+
+	// Add a type field to all events handled by this input
+	Type string
+}
+
+func (p *processor) Configure(ctx processors.ProcessorContext, conf map[string]interface{}) error {
+	p.opt.Congestion_threshold = 30
+	p.opt.Host = "0.0.0.0"
+	p.opt.Port = 5044
+	p.opt.Ssl = false
+	p.opt.Ssl_verify_mode = "none"
+
+	return p.ConfigureAndValidate(ctx, conf, p.opt)
+}
+
+func (p *processor) Start(e processors.IPacket) error {
+	p.q = make(chan bool)
+
+	options := []v2.Option{}
+
+	if p.opt.Ssl == true {
+		config := &tls.Config{}
+
+		// Server Certificates
+		cert, err := tls.LoadX509KeyPair(p.opt.Ssl_certificate, p.opt.Ssl_key)
+
 		if err != nil {
-			if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
-				continue
-			}
-			// log.Printf("Error accepting connection: %v", err)
-			continue
+			return fmt.Errorf("Error loading keys: %v", err)
 		}
+		config.Certificates = []tls.Certificate{cert}
 
-		if p.opt.Ssl == true {
-			config := tls.Config{}
-
-			// Server Certificates
-			cert, err := tls.LoadX509KeyPair(p.opt.Ssl_certificate, p.opt.Ssl_key)
-
-			if err != nil {
-				return fmt.Errorf("Error loading keys: %v", err)
-			}
-			config.Certificates = []tls.Certificate{cert}
-
-			// Certificate authority
-			if len(p.opt.Ssl_certificate_authorities) > 0 {
-				config.RootCAs = x509.NewCertPool()
-				for _, pemCertPath := range p.opt.Ssl_certificate_authorities {
-					pemCert, err := ioutil.ReadFile(pemCertPath)
-					if err != nil {
-						return fmt.Errorf("Error loading certificate authorities: %v", err)
-					}
-					config.RootCAs.AppendCertsFromPEM(pemCert)
+		// Certificate authority
+		if len(p.opt.Ssl_certificate_authorities) > 0 {
+			config.RootCAs = x509.NewCertPool()
+			for _, pemCertPath := range p.opt.Ssl_certificate_authorities {
+				pemCert, err := ioutil.ReadFile(pemCertPath)
+				if err != nil {
+					return fmt.Errorf("Error loading certificate authorities: %v", err)
 				}
+				config.RootCAs.AppendCertsFromPEM(pemCert)
 			}
-
-			// SSL Verification mode
-			if p.opt.Ssl_verify_mode == "peer" {
-				config.ClientAuth = tls.VerifyClientCertIfGiven
-			}
-			if p.opt.Ssl_verify_mode == "force_peer" {
-				config.ClientAuth = tls.RequireAndVerifyClientCert
-			}
-
-			conn = tls.Server(conn, &config)
 		}
 
-		wg.Add(1)
-		go p.clientServe(conn, &wg, clientTerm)
+		// SSL Verification mode
+		if p.opt.Ssl_verify_mode == "peer" {
+			config.ClientAuth = tls.VerifyClientCertIfGiven
+		}
+		if p.opt.Ssl_verify_mode == "force_peer" {
+			config.ClientAuth = tls.RequireAndVerifyClientCert
+		}
+
+		options = append(options, v2.TLS(config))
 	}
+
+	options = append(options, v2.Timeout(time.Second*time.Duration(p.opt.Congestion_threshold)))
+
+	log.Logger = p.Logger
+	server, err := v2.ListenAndServe(fmt.Sprintf("%s:%d", p.opt.Host, p.opt.Port), options...)
+	if err != nil {
+		return err
+	}
+
+	p.server = server
+
+	go func() {
+		for batch := range p.server.ReceiveChan() {
+			batch.ACK()
+			events := batch.Events
+			for _, e := range events {
+				fields := e.(map[string]interface{})
+				if val, ok := fields["@timestamp"]; !ok {
+					fields["@timestamp"] = time.Now()
+				} else {
+					fields["@timestamp"], _ = time.Parse("2006-01-02T15:04:05Z07:00", val.(string))
+				}
+
+				ev := p.NewPacket("", fields)
+				processors.ProcessCommonFields(ev.Fields(), p.opt.Add_field, p.opt.Tags, p.opt.Type)
+				p.Send(ev, 0)
+			}
+		}
+		p.Logger.Debug("received events acked and drained")
+		close(p.q)
+	}()
 
 	return nil
 }
 
-// lumberConn handles an incoming connection from a lumberjack client
-func (p *processor) clientServe(c net.Conn, wg *sync.WaitGroup, clientTerm chan bool) {
-	defer wg.Done()
-	defer c.Close()
-
-	// log.Printf("[%s] accepting lumberjack connection", c.RemoteAddr().String())
-
-	dataChan := make(chan map[string]interface{}, 3)
-	go NewParser(c, dataChan, p.Logger).Parse()
-
-	for {
-		select {
-		case fields := <-dataChan:
-			if fields == nil {
-				// log.Printf("[%s] closing lumberjack connection", c.RemoteAddr().String())
-				return
-			}
-			msg := ""
-			if txt, ok := fields["message"]; ok {
-				msg = txt.(string)
-			}
-			e := p.NewPacket(msg, fields)
-			processors.ProcessCommonFields(e.Fields(), p.opt.Add_field, p.opt.Tags, p.opt.Type)
-			p.Send(e)
-		case <-clientTerm:
-			c.SetReadDeadline(time.Now())
-		}
+func (p *processor) Stop(e processors.IPacket) error {
+	err := p.server.Close()
+	if err != nil {
+		return err
 	}
-
+	<-p.q
+	return nil
 }
