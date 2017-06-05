e3434f54cedb73c4de1b5c51e0056ecf6ddffb8a
Valere JEANTET <valere.jeantet@gmail.com>
2017-06-05 22:53:46 +0200

    codecs : line - separate encoder and decoder options

---
+++ codecs/line/decoder.go
@@ -0,0 +1,80 @@
+//go:generate bitfanDoc -codec decoder
+package linecodec
+
+import (
+	"bufio"
+	"io"
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+type decoder struct {
+	more    bool
+	r       *bufio.Scanner
+	options decoderOptions
+}
+
+type decoderOptions struct {
+	// Change the delimiter that separates lines
+	// @Default "\\n"
+	Delimiter string
+}
+
+func NewDecoder(r io.Reader) *decoder {
+	d := &decoder{
+		r:    bufio.NewScanner(r),
+		more: true,
+		options: decoderOptions{
+			Delimiter: "\n",
+		},
+	}
+
+	split := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		// Return nothing if at end of file and no data passed
+		if atEOF && len(data) == 0 {
+			return 0, nil, nil
+		}
+
+		// Find the index of the input of a newline followed by a
+		// pound sign.
+		if i := strings.Index(string(data), d.options.Delimiter); i >= 0 {
+			return i + 1, data[0:i], nil
+		}
+
+		// If at end of file with data return the data
+		if atEOF {
+			return len(data), data, nil
+		}
+		// Request more data.
+		return 0, nil, nil
+	}
+
+	d.r.Split(split)
+
+	return d
+}
+
+func (d *decoder) SetOptions(conf map[string]interface{}) error {
+	if err := mapstructure.Decode(conf, &d.options); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *decoder) Decode() (map[string]interface{}, error) {
+	data := map[string]interface{}{}
+	if true == d.r.Scan() {
+		d.more = true
+		data["message"] = d.r.Text()
+	} else {
+		d.more = false
+		return data, io.EOF
+	}
+
+	return data, nil
+}
+
+func (d *decoder) More() bool {
+	return d.more
+}
--- codecs/line/encoder.go
+++ codecs/line/encoder.go
@@ -1,12 +1,10 @@
-//go:generate bitfanDoc -codec line
+//go:generate bitfanDoc -codec encoder
 package linecodec
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
 	"io"
-	"strings"
 	"text/template"
 	"time"
 
@@ -15,24 +13,18 @@ import (
 
 const timeFormat = "2006-01-02T15:04:05.999Z07:00"
 
-type decoder struct {
-	more    bool
-	r       *bufio.Scanner
-	options options
-}
-
 type encoder struct {
 	w         io.Writer
-	options   options
+	options   encoderOptions
 	formatTmp *template.Template
 }
 
-type options struct {
+type encoderOptions struct {
 	// Change the delimiter that separates lines
 	// @Default "\\n"
 	Delimiter string
 
-	// Format (Encoder) as a golang text/template
+	// Format as a golang text/template
 	Format string `mapstructure:"format"`
 
 	// You can set variable to be used in Statements by using ${var}.
@@ -45,7 +37,7 @@ type options struct {
 func NewEncoder(w io.Writer) *encoder {
 	e := &encoder{
 		w: w,
-		options: options{
+		options: encoderOptions{
 			Delimiter: "\n",
 			Format:    "{{Timestamp .}} {{.host}} {{.message}}\n",
 		},
@@ -94,63 +86,6 @@ func (e *encoder) Encode(data map[string]interface{}) error {
 	buff := bytes.NewBufferString("")
 	e.formatTmp.Execute(buff, data)
 	e.w.Write(buff.Bytes())
+	e.w.Write([]byte(e.options.Delimiter))
 	return nil
-}
-
-func NewDecoder(r io.Reader) *decoder {
-	d := &decoder{
-		r:    bufio.NewScanner(r),
-		more: true,
-		options: options{
-			Delimiter: "\n",
-		},
-	}
-
-	split := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-		// Return nothing if at end of file and no data passed
-		if atEOF && len(data) == 0 {
-			return 0, nil, nil
-		}
-
-		// Find the index of the input of a newline followed by a
-		// pound sign.
-		if i := strings.Index(string(data), d.options.Delimiter); i >= 0 {
-			return i + 1, data[0:i], nil
-		}
-
-		// If at end of file with data return the data
-		if atEOF {
-			return len(data), data, nil
-		}
-		// Request more data.
-		return 0, nil, nil
-	}
-
-	d.r.Split(split)
-
-	return d
-}
-
-func (d *decoder) SetOptions(conf map[string]interface{}) error {
-	if err := mapstructure.Decode(conf, &d.options); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (d *decoder) Decode() (map[string]interface{}, error) {
-	data := map[string]interface{}{}
-	if true == d.r.Scan() {
-		d.more = true
-		data["message"] = d.r.Text()
-	} else {
-		d.more = false
-		return data, io.EOF
-	}
-
-	return data, nil
-}
-
-func (d *decoder) More() bool {
-	return d.more
 }
