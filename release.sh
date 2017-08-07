7134d1f6f575959d79c8730a142885ebc097b13a
Valere JEANTET <valere.jeantet@gmail.com>
2017-08-07 22:27:22 +0200

    change build arch order

--- release.sh
+++ release.sh
@@ -9,7 +9,22 @@ rm releases/*.zip
 go generate .
 
 arch=amd64
+os=windows
+product=${productName}_v${versionLabel}_${os}_${arch}
+env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o releases/${product}.exe .
+zip -r releases/${product}.zip releases/${product}.exe
+rm releases/$product.exe
+
+arch=amd64
 os=darwin
+product=${productName}_v${versionLabel}_${os}_${arch}
+env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o releases/$product .
+tar czfv releases/$product.tgz releases/$product
+rm releases/$product
+
+
+arch=arm
+os=linux
 product=${productName}_v${versionLabel}_${os}_${arch}
 env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o releases/$product .
 tar czfv releases/$product.tgz releases/$product
@@ -28,20 +43,6 @@ rm releases/$product
 #env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o releases/$product .
 #tar czfv releases/$product.tgz releases/$product
 #rm releases/$product
-
-arch=arm
-os=linux
-product=${productName}_v${versionLabel}_${os}_${arch}
-env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o releases/$product .
-tar czfv releases/$product.tgz releases/$product
-rm releases/$product
-
-arch=amd64
-os=windows
-product=${productName}_v${versionLabel}_${os}_${arch}
-env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o releases/${product}.exe .
-zip -r releases/${product}.zip releases/${product}.exe
-rm releases/$product.exe
 
 #arch=386
 #os=windows
