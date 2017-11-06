fee4085e2dc051e663c5bd26c77e1f99416f947d
Valere JEANTET <valere.jeantet@gmail.com>
2017-11-06 22:46:13 +0100

    core : store - replace SQLite by Bolt

--- core/store.go
+++ core/store.go
@@ -3,23 +3,53 @@ package core
 import (
 	"fmt"
 	"path/filepath"
+	"time"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/timshannon/bolthold"
 	"github.com/vjeantet/bitfan/core/models"
 )
 
 type Store struct {
-	db *gorm.DB
+	db *bolthold.Store
+}
+
+type StorePipeline struct {
+	Uuid string `json:"uuid" boltholdKey:"Uuid"`
+
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	StartedAt time.Time `json:"started_at"`
+
+	Label       string `json:"label"`
+	Description string `json:"description"`
+	AutoStart   bool   `json:"auto_start" boltholdIndex:"AutoStart" mapstructure:"auto_start"`
+
+	// Assets
+	Assets []StoreAssetRef `json:"assets"`
+}
+
+type StoreAssetRef struct {
+	Uuid  string `json:"uuid" boltholdIndex:"PipelineAssetUUID"`
+	Label string
+	Type  string
+}
+
+type StoreAsset struct {
+	Uuid      string    `json:"uuid" boltholdKey:"Uuid"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+
+	PipelineUUID string `boltholdIndex:"AssetPipelineUUID"`
+
+	Label       string
+	Type        string `boltholdIndex:"AssetType"`
+	ContentType string
+	Value       []byte
+	Size        int
 }
 
 func NewStore(location string) (*Store, error) {
-	database, err := gorm.Open("sqlite3", filepath.Join(location, "bitfan.db"))
-	// database.LogMode(true)
-
-	// Migrate the schema
-	database.AutoMigrate(&models.Pipeline{}, &models.Asset{})
-
+	database, err := bolthold.Open(filepath.Join(location, "bitfan.bolt.db"), 0666, nil)
 	return &Store{db: database}, err
 }
 
@@ -28,54 +58,303 @@ func (s *Store) close() {
 }
 
 func (s *Store) FindPipelinesWithAutoStart() []models.Pipeline {
-	pipelinesToStart := []models.Pipeline{}
-	s.db.Where(&models.Pipeline{AutoStart: true}).Find(&pipelinesToStart).RecordNotFound()
-	return pipelinesToStart
+	pps := []models.Pipeline{}
+
+	var sps []StorePipeline
+	err := s.db.Find(&sps, bolthold.Where("AutoStart").Eq(true))
+	if err != nil {
+		Log().Errorf("Store : FindPipelinesWithAutoStart - %s", err.Error())
+		return pps
+	}
+
+	for _, p := range sps {
+		var tPipeline models.Pipeline
+		tPipeline.Uuid = p.Uuid
+		tPipeline.CreatedAt = p.CreatedAt
+		tPipeline.UpdatedAt = p.UpdatedAt
+		tPipeline.StartedAt = p.StartedAt
+		tPipeline.Label = p.Label
+		tPipeline.Description = p.Description
+		tPipeline.AutoStart = p.AutoStart
+
+		for _, a := range p.Assets {
+			tPipeline.Assets = append(tPipeline.Assets, models.Asset{
+				Uuid: a.Uuid,
+				Name: a.Label,
+				Type: a.Type,
+			})
+		}
+		pps = append(pps, tPipeline)
+	}
+
+	return pps
 }
 
 func (s *Store) FindOnePipelineByUUID(UUID string) (models.Pipeline, error) {
 	tPipeline := models.Pipeline{Uuid: UUID}
-	if s.db.Preload("Assets").Where(&tPipeline).First(&tPipeline).RecordNotFound() {
+
+	var sps []StorePipeline
+	err := s.db.Find(&sps, bolthold.Where(bolthold.Key).Eq(UUID))
+	if err != nil {
+		return tPipeline, err
+	}
+	if len(sps) == 0 {
 		return tPipeline, fmt.Errorf("Pipeline %s not found", UUID)
 	}
+
+	tPipeline.CreatedAt = sps[0].CreatedAt
+	tPipeline.UpdatedAt = sps[0].UpdatedAt
+	tPipeline.StartedAt = sps[0].StartedAt
+	tPipeline.Label = sps[0].Label
+	tPipeline.Description = sps[0].Description
+	tPipeline.AutoStart = sps[0].AutoStart
+
+	for _, a := range sps[0].Assets {
+		tPipeline.Assets = append(tPipeline.Assets, models.Asset{
+			Uuid: a.Uuid,
+			Name: a.Label,
+			Type: a.Type,
+		})
+	}
+
 	return tPipeline, nil
 }
 
 func (s *Store) CreatePipeline(p *models.Pipeline) {
-	s.db.Create(p)
+	sp := &StorePipeline{
+		Uuid:        p.Uuid,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+		Label:       p.Label,
+		Description: p.Description,
+		AutoStart:   p.AutoStart,
+	}
+
+	for _, a := range p.Assets {
+		sp.Assets = append(sp.Assets,
+			StoreAssetRef{
+				Uuid:  a.Uuid,
+				Label: a.Name,
+				Type:  a.Type,
+			})
+		sav := &StoreAsset{
+			Uuid:         a.Uuid,
+			CreatedAt:    time.Now(),
+			UpdatedAt:    time.Now(),
+			PipelineUUID: sp.Uuid,
+			Label:        a.Name,
+			Type:         a.Type,
+			ContentType:  a.ContentType,
+			Value:        a.Value,
+			Size:         a.Size,
+		}
+		s.db.Upsert(sav.Uuid, sav)
+	}
+
+	s.db.Upsert(sp.Uuid, sp)
 }
 
 func (s *Store) SavePipeline(p *models.Pipeline) {
-	s.db.Save(p)
+	sp := &StorePipeline{
+		Uuid:        p.Uuid,
+		CreatedAt:   p.CreatedAt,
+		UpdatedAt:   time.Now(),
+		Label:       p.Label,
+		Description: p.Description,
+		AutoStart:   p.AutoStart,
+	}
+
+	for _, a := range p.Assets {
+		sp.Assets = append(sp.Assets,
+			StoreAssetRef{
+				Uuid:  a.Uuid,
+				Label: a.Name,
+				Type:  a.Type,
+			})
+		sav := &StoreAsset{
+			Uuid:         a.Uuid,
+			CreatedAt:    time.Now(),
+			UpdatedAt:    time.Now(),
+			PipelineUUID: sp.Uuid,
+			Label:        a.Name,
+			Type:         a.Type,
+			ContentType:  a.ContentType,
+			Value:        a.Value,
+			Size:         a.Size,
+		}
+		s.db.Upsert(sav.Uuid, sav)
+	}
+
+	s.db.Upsert(sp.Uuid, sp)
 }
 
 func (s *Store) DeletePipeline(p *models.Pipeline) {
-	s.db.Delete(models.Asset{}, "pipeline_uuid = ?", p.Uuid)
-	s.db.Delete(&p)
+	err := s.db.DeleteMatching(&StoreAsset{}, bolthold.Where("PipelineUUID").Eq(p.Uuid))
+	if err != nil {
+		Log().Errorf("Store : DeletePipeline - %s", err.Error())
+		return
+	}
+
+	err = s.db.Delete(p.Uuid, &StorePipeline{})
+	if err != nil {
+		Log().Errorf("Store : DeletePipeline - %s", err.Error())
+		return
+	}
+
 }
 
 func (s *Store) FindPipelines() []models.Pipeline {
 	pps := []models.Pipeline{}
-	s.db.Find(&pps)
+
+	var sps []StorePipeline
+	err := s.db.Find(&sps, &bolthold.Query{})
+	if err != nil {
+		Log().Errorf("Store : FindPipelines - %s", err.Error())
+		return pps
+	}
+
+	for _, p := range sps {
+		var tPipeline models.Pipeline
+		tPipeline.Uuid = p.Uuid
+		tPipeline.CreatedAt = p.CreatedAt
+		tPipeline.UpdatedAt = p.UpdatedAt
+		tPipeline.StartedAt = p.StartedAt
+		tPipeline.Label = p.Label
+		tPipeline.Description = p.Description
+		tPipeline.AutoStart = p.AutoStart
+
+		for _, a := range p.Assets {
+			tPipeline.Assets = append(tPipeline.Assets, models.Asset{
+				Uuid: a.Uuid,
+				Name: a.Label,
+				Type: a.Type,
+			})
+		}
+		pps = append(pps, tPipeline)
+	}
+
 	return pps
 }
 
 func (s *Store) CreateAsset(a *models.Asset) {
-	s.db.Create(a)
+	sav := &StoreAsset{
+		Uuid:         a.Uuid,
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+		PipelineUUID: a.PipelineUUID,
+		Label:        a.Name,
+		Type:         a.Type,
+		ContentType:  a.ContentType,
+		Value:        a.Value,
+		Size:         a.Size,
+	}
+	s.db.Upsert(sav.Uuid, sav)
+
+	var sps []StorePipeline
+	err := s.db.Find(&sps, bolthold.Where(bolthold.Key).Eq(a.PipelineUUID))
+	if err != nil {
+		Log().Errorf("Store : createAsset - %s", err.Error())
+		return
+	}
+	if len(sps) == 0 {
+		Log().Errorf("Store : createAsset - can not find pipeline (%s)", a.PipelineUUID)
+		return
+	}
+
+	sps[0].Assets = append(sps[0].Assets, StoreAssetRef{
+		Uuid:  a.Uuid,
+		Label: a.Name,
+		Type:  a.Type,
+	})
+	s.db.Upsert(sps[0].Uuid, sps[0])
+
 }
 
 func (s *Store) SaveAsset(a *models.Asset) {
-	s.db.Save(a)
+	sav := &StoreAsset{
+		Uuid:         a.Uuid,
+		CreatedAt:    a.CreatedAt,
+		UpdatedAt:    time.Now(),
+		PipelineUUID: a.PipelineUUID,
+		Label:        a.Name,
+		Type:         a.Type,
+		ContentType:  a.ContentType,
+		Value:        a.Value,
+		Size:         a.Size,
+	}
+	s.db.Upsert(sav.Uuid, sav)
+
+	var sps []StorePipeline
+	err := s.db.Find(&sps, bolthold.Where(bolthold.Key).Eq(a.PipelineUUID))
+	if err != nil {
+		Log().Errorf("Store : SaveAsset - %s", err.Error())
+		return
+	}
+	if len(sps) == 0 {
+		Log().Errorf("Store : SaveAsset - can not find pipeline (%s)", a.PipelineUUID)
+		return
+	}
+
+	for i, _ := range sps[0].Assets {
+		if sps[0].Assets[i].Uuid == a.Uuid {
+			sps[0].Assets[i].Label = a.Name
+			sps[0].Assets[i].Type = a.Type
+		}
+	}
+	s.db.Upsert(sps[0].Uuid, sps[0])
+
 }
 
 func (s *Store) DeleteAsset(a *models.Asset) {
-	s.db.Delete(&a)
+	err := s.db.Delete(a.Uuid, &StoreAsset{})
+	if err != nil {
+		Log().Errorf("Store : DeleteAsset - %s", err.Error())
+		return
+	}
+
+	var sps []StorePipeline
+	err = s.db.Find(&sps, bolthold.Where(bolthold.Key).Eq(a.PipelineUUID))
+	if err != nil {
+		Log().Errorf("Store : DeleteAsset - %s", err.Error())
+		return
+	}
+	if len(sps) == 0 {
+		Log().Errorf("Store : DeleteAsset - can not find pipeline (%s)", a.PipelineUUID)
+		return
+	}
+
+	sars := []StoreAssetRef{}
+	for _, ar := range sps[0].Assets {
+		if ar.Uuid != a.Uuid {
+			sars = append(sars, ar)
+		}
+	}
+	sps[0].Assets = sars
+	s.db.Upsert(sps[0].Uuid, sps[0])
+
 }
 
 func (s *Store) FindOneAssetByUUID(uuid string) (models.Asset, error) {
 	asset := models.Asset{Uuid: uuid}
-	if s.db.Where(&asset).First(&asset).RecordNotFound() {
+
+	var sas []StoreAsset
+	err := s.db.Find(&sas, bolthold.Where(bolthold.Key).Eq(uuid))
+	if err != nil {
+		return asset, err
+	}
+	if len(sas) == 0 {
 		return asset, fmt.Errorf("Asset %s not found", uuid)
 	}
+
+	asset.ContentType = sas[0].ContentType
+	asset.CreatedAt = sas[0].CreatedAt
+	asset.Name = sas[0].Label
+	asset.PipelineUUID = sas[0].PipelineUUID
+	asset.Size = sas[0].Size
+	asset.Type = sas[0].Type
+	asset.UpdatedAt = sas[0].UpdatedAt
+	asset.Uuid = sas[0].Uuid
+	asset.Value = sas[0].Value
+
 	return asset, nil
 }
