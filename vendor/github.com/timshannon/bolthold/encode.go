987167bff80f9284205c6d329e5329b391dab24b
Valere JEANTET <valere.jeantet@gmail.com>
2017-11-06 22:54:07 +0100

    vendoring : add github.com/timshannon/bolthold

---
+++ vendor/github.com/timshannon/bolthold/encode.go
@@ -0,0 +1,51 @@
+// Copyright 2016 Tim Shannon. All rights reserved.
+// Use of this source code is governed by the MIT license
+// that can be found in the LICENSE file.
+
+package bolthold
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+// EncodeFunc is a function for encoding a value into bytes
+type EncodeFunc func(value interface{}) ([]byte, error)
+
+// DecodeFunc is a function for decoding a value from bytes
+type DecodeFunc func(data []byte, value interface{}) error
+
+var encode EncodeFunc
+var decode DecodeFunc
+
+// DefaultEncode is the default encoding func for bolthold (Gob)
+func DefaultEncode(value interface{}) ([]byte, error) {
+	var buff bytes.Buffer
+
+	en := gob.NewEncoder(&buff)
+
+	err := en.Encode(value)
+	if err != nil {
+		return nil, err
+	}
+
+	return buff.Bytes(), nil
+}
+
+// DefaultDecode is the default decoding func for bolthold (Gob)
+func DefaultDecode(data []byte, value interface{}) error {
+	var buff bytes.Buffer
+	de := gob.NewDecoder(&buff)
+
+	_, err := buff.Write(data)
+	if err != nil {
+		return err
+	}
+
+	err = de.Decode(value)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
