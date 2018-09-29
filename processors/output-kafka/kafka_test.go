a0907c1c1ee1bd790f76b4cd09b32b56795bfef3
Alan Willis <alan@amekoshi.com>
2018-09-28 23:46:10 -0700

    add test for bootstrap_servers lookup

--- processors/output-kafka/kafka_test.go
+++ processors/output-kafka/kafka_test.go
@@ -22,3 +22,8 @@ func TestConfigure(t *testing.T) {
 	err := p.Configure(ctx, conf)
 	assert.Nil(t, err, "Configure() processor without error")
 }
+func TestBootstrapLookup(t *testing.T)  {
+	brokers, err := bootstrapLookup("codecov.io", "1000")
+	assert.Nil(t, err)
+	assert.True(t, len(brokers) > 0)
+}
